17a8f7fac6729df9c7a44618b3ea834c5e3842d2
houyun <houyunhuang@163.com>
2021-03-19 21:09:41 +0800

    copy from ggcor

--- R/hyplot.R
+++ R/hyplot.R
@@ -47,3 +47,42 @@ hyplot <- function(md, mapping = NULL, ...) {
   class(p) <- c("hyplot", class(p))
   p
 }
+
+#' @importFrom ggplot2 coord_polar
+hyplot_circular <- function(md,
+                            mapping = NULL,
+                            open = 45,
+                            inner = 1,
+                            outer = 0.2,
+                            ...) {
+  if (!is_matrix_data(md) && !is_md_tbl(md)) {
+    if (!"name" %in% names(list(...))) {
+      nm <- deparse(substitute(md))
+      md <- as_matrix_data(md, name = nm, ...)
+    } else {
+      md <- as_matrix_data(md, ...)
+    }
+  }
+
+  if (is_matrix_data(md)) {
+    md <- fortify(md, ...)
+  }
+
+  base_mapping <- aes_(x = ~.colnames, y = ~.rownames)
+  if (is.null(mapping)) {
+    mapping <- base_mapping
+  } else {
+    mapping <- modifyList(base_mapping, mapping)
+  }
+
+  p <- ggplot(data = md,
+              mapping = mapping)
+  p <- p + scale_x_discrete(limits = col_names(md)) +
+    scale_y_discrete(limits = rev(row_names(md))) +
+    coord_polar()
+
+  # adjust the default theme
+  p <- p + theme_hy()
+  class(p) <- c("hyplot", class(p))
+  p
+}
